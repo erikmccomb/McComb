2a8d12dd2f81e50c204e49d7eef729235fccf818
mccomb <erik.k.mccomb@gmail.com>
2018-02-13 21:00:19 -0700

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { 
+  BrowserRouter as Router, 
+  Route,
+} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <Router>
+      <div>
+        <Route exactly pattern="/" component={App}/>
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
