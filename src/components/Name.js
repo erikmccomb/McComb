a23b247bf7bea7254883ce490b230c46e67c6c55
mccomb <erik.k.mccomb@gmail.com>
2018-04-11 01:03:34 -0600

    subrow fixed mobile and desktop

--- src/components/Name.js
+++ src/components/Name.js
@@ -57,8 +57,6 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'space-between',
     overflow: 'hidden',
-    minWidth: '50vw',
-    maxWidth: '55vw',
     fontFamily: 'Calendas_Plus',
     fontSize: 'calc(10px + 1vw)',
     lineHeight: 'calc(10px + 1vw)',
