5d83993f37b55129af64c1f00de1e54635e63f16
mccomb <erik.k.mccomb@gmail.com>
2018-05-25 11:14:37 -0600

    changed from menu to sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,56 @@ import NavBar from './NavBar'
 import Main from './Main'
 import Flexbox from 'flexbox-react'
 import '../css/App.css'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 
-const App = () => (
-  <Flexbox 
-    flexDirection='column' 
-    minHeight='100vh' 
-    maxWidth='100vw'
-  >
-    <Flexbox>
-      <NavBar />
-    </Flexbox>
-    <Flexbox flexGrow={1}>
-      <Main />
-    </Flexbox>
-  </Flexbox>
-)
+class App extends React.Component {
+  state = { visible: false }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  render() {
+    const { visible } = this.state
+    return (
+      <Flexbox 
+        flexDirection='column' 
+        minHeight='100vh' 
+        maxWidth='100vw'
+        style={{backgroundColor:'#131515'}}
+      >
+        <Sidebar.Pushable as={Segment} style={{backgroundColor:'#131515'}}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            width='thin'
+            direction='right'
+            visible={visible}
+            icon='labeled'
+            vertical
+            inverted
+          >
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item name='gamepad'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item name='camera'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Icon link size='big' name='content' onClick={this.toggleVisibility} style={{padding:'10px', color: '#7de2d1'}} />
+            <Flexbox>
+              <Main/>
+            </Flexbox>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Flexbox>
+    )
+  }
+}
 
 export default App
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,71 +1,15 @@
 import React from 'react'
-import { Menu, Icon } from 'semantic-ui-react'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 
 class NavBar extends React.Component {
   render() {
     return (
-      <Menu style={{width: '100vw', maxWidth:'100vw'}}>
+      <Menu borderless style={{width: '100vw', maxWidth:'100vw'}}>
         <Menu.Item name='Mccomb'>
-          <Link to='/'>
-            McComb
-          </Link>
+
         </Menu.Item>
-
-        <Menu.Item name='Portfolio'>
-          <Link to='/Portfolio'>
-            Portfolio
-          </Link>
-        </Menu.Item>
-
-        
-        <Menu.Item name='Blog'>
-          <a href="https://erikmccombblog.netlify.com/" 
-            target="_blank" 
-            rel="noopener noreferrer"
-          >
-            Blog
-          </a>
-        </Menu.Item>
-
-        <Menu.Menu position='right'>
-          <Menu.Item name='Github' >
-            <a href="http://github.com/erikmccomb" 
-              target="_blank" 
-              rel="noopener noreferrer" 
-            >
-              <Icon name='github' size='large' />
-            </a>
-          </Menu.Item>
-
-          <Menu.Item name='Linkedin' >
-            <a href="https://www.linkedin.com/in/erikmccomb/" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              <Icon name='linkedin' size='large' />
-            </a>
-          </Menu.Item>
-
-          <Menu.Item name='Twitter' >
-            <a href="https://twitter.com/Erik_the_Mick" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              <Icon name='twitter' size='large' />
-            </a>
-          </Menu.Item>
-
-          <Menu.Item name='Instagram'>
-            <a href="http://www.instagram.com/itserikwith_a_k/" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              <Icon name='instagram' size='large' />
-            </a>
-          </Menu.Item>
-        </Menu.Menu>
       </Menu>
     )
   }
